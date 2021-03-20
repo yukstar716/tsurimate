460f018141cffa9fade0fd46758c69cc92a9d4da
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-03-20 13:12:27 +0900

    create top page layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
+import 'app/assets/stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
