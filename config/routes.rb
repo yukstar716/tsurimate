a82f22210464f3a0ea598ae26e568acaaceb0a41
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-03-30 22:25:41 +0900

    post → postsに変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
   get 'top',   to: 'static_pages#top'  
   get 'about', to: 'static_pages#about'
-  get 'post',  to: 'post#new'
+  get 'posts',  to: 'posts#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
   
