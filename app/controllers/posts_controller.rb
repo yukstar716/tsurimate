e1745b9134b2758db5aa6e835a38eafa9864a528
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-04-01 08:46:59 +0900

    Post modelのcreateアクション作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
+  #before_action :logged_in_user, only: [:create, :destroy]
+  
   def new
+  end
+  
+  def create
+    @post = current_user.posts.build(posts_params)
+    if @post.save
+      flash[:success] = "投稿しました！"
+      redirect_to top_path
+    else
+      render 'posts/new'
+    end
+  end
+
+  def destroy
+  end
+
+  def posts_params
+    params.require(:posts).permit(:title, :content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_235149) do
+ActiveRecord::Schema.define(version: 2021_03_31_232205) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_03_27_235149) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
