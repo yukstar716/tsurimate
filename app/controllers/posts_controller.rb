ea83e0fe3231356dcb34e13c30d8f915ebf5e398
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-04-04 18:36:52 +0900

    投稿データをtopページに表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
   #before_action :logged_in_user, only: [:create, :destroy]
   
+  def index
+    #@posts = Post.all.order(created_at: "desc")
+  end
+
   def new
+    @post = Post.new
   end
   
   def create
-    @post = current_user.posts.build(posts_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "投稿しました！"
       redirect_to top_path
@@ -14,10 +19,14 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.all
+  end
+
   def destroy
   end
 
-  def posts_params
-    params.require(:posts).permit(:title, :content)
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def top
+    @posts = Post.all.order(created_at: "desc")
   end
 
   def about
