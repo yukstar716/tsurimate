7de25faa3e14a83f009c7e74bf221a9fe0866362
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-04-14 00:09:30 +0900

    プロフィールページのレイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { 
-    :omniauth_callbacks => "omniauth_callbacks", 
+    :omniauth_callbacks => "omniauth_callbacks",
   }
   get 'top',   to: 'posts#index'  
   get 'about', to: 'static_pages#about'
