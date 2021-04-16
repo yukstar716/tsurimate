3a59424f0a8a7935648a2e44c800f334c8a6f8b8
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-04-17 00:03:20 +0900

    user/idにそのユーザーのポスト一覧を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @posts = Post.all.order(created_at: "desc")
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'posts',  to: 'posts#show'
   get 'users',  to: 'users#show'
   resources :posts
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
   
