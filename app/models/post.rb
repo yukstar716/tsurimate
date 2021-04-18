82665f0134aa3a80a5aeb127253419aef0aa7291
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-04-19 00:00:09 +0900

    コメント機能のコントローラー作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comment_to_posts, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 300 }
 end
