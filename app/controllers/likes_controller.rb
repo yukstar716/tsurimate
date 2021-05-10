b2cfb01d09454e26ec3fdd5d9dc0c92eb6a07421
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-05-10 09:48:21 +0900

    いいねクリック時にカウントを表示/いいねが反映されないバグを改修

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,15 +3,18 @@ class LikesController < ApplicationController
   before_action :set_like
 
   def create
-    #user = current_user
-    #post = Post.find(parasms[:post_id])
-    Like.create(user_id: current_user.id, post_id: params[:id])
+    user = current_user
+    post = Post.find(params[:post_id])
+    #byebug
+    like = Like.create(user_id: current_user.id, post_id: post.id)
+    #redirect_to controller: :posts, action: :show
   end
 
   def destroy
-    #user = current_user
-    #post = Post.find(params[:post_id])
-    Like.find_by(user_id: current_user.id, post_id: params[:id]).delete
+    user = current_user
+    post = Post.find(params[:post_id])
+    Like.find_by(user_id: current_user.id, post_id: post.id)
+    like.delete
   end
 
   private
