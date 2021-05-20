d75df3478214dceedf768ce7fb8618c6a6ffff44
Yuki Matsumoto <yumatsu.july19920716@gmail.com>
2021-05-20 09:28:58 +0900

    post controllerのテスト

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "#index" do
+    before do
+      @user = User.new(
+        username: "hoge",
+        email:    "hoge@g.com",
+        password: "hogeho",
+      )
+
+      @post = @user.posts.create(
+        title:   "this is test",
+        content: "this is content test",
+      )
+    end
+
     it "responds successfully" do
       get "/top"
       expect(response).to have_http_status(:success)
